4c19275cce6d57ab014197489fb617da6c820305
Gabriel Brand <gabr.brand@gmail.com>
2022-06-14 17:47:33 +0200

    Add bg.png

--- dot_config/awesome/awesome-wm-widgets/logout-menu-widget/logout-menu.lua
+++ dot_config/awesome/awesome-wm-widgets/logout-menu-widget/logout-menu.lua
@@ -52,7 +52,7 @@ local function worker(user_args)
     local font = args.font or beautiful.font
 
     local onlogout = args.onlogout or function () awesome.quit() end
-    local onlock = args.onlock or function() awful.spawn.with_shell("i3lock -i /home/gabriel/.bg.png -t") end
+    local onlock = args.onlock or function() awful.spawn.with_shell("i3lock -i /home/gabriel/.config/bg.png -t") end
     local onreboot = args.onreboot or function() awful.spawn.with_shell("reboot") end
     local onsuspend = args.onsuspend or function() awful.spawn.with_shell("systemctl suspend") end
     local onpoweroff = args.onpoweroff or function() awful.spawn.with_shell("shutdown now") end
